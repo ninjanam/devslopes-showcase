a20163f093f46f9af53622c44a3738165a8f8e10
Nam-Anh Vu <Nam-Anh@nam-anhs-mbp.attlocal.net>
2016-10-23 21:31:54 -0500

    email and password authentication complete

--- devslopes-showcase/Constants.swift
+++ devslopes-showcase/Constants.swift
@@ -10,7 +10,13 @@ import Foundation
 import UIKit
 
 let SHADOW_COLOR: CGFloat = 157.0 / 255.0
+
+// Keys
 let KEY_ID = "uid"
 
 // Segues
 let SEGUE_LOGGED_IN = "goToFeed"
+
+// Status codes
+let STATUS_ACCOUNT_NONEXIST = 17011
+let STATUS_WRONG_PASSWORD = 17009
--- devslopes-showcase/SignInVC.swift
+++ devslopes-showcase/SignInVC.swift
@@ -13,6 +13,9 @@ import Firebase
 
 class SignInVC: UIViewController {
 
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -65,5 +68,55 @@ class SignInVC: UIViewController {
             }
         })
     }
+    
+    @IBAction func attemptLogin(sender: UIButton) {
+        
+        if let email = emailField.text where email != "", let pwd = passwordField.text where pwd != "" {
+            
+            FIRAuth.auth()?.signInWithEmail(email, password: pwd, completion: { (user, error) -> Void in
+                
+                if error != nil {
+                    print(error)
+                    if error!.code == STATUS_ACCOUNT_NONEXIST {
+                        FIRAuth.auth()?.createUserWithEmail(email, password: pwd, completion: { (user, error) -> Void in
+                            if error != nil {
+                                print("Unable to authenticate new user with Firebase")
+                                self.showErrorAlert("Could not create account", msg: "Problem creating account. Try something else")
+                            } else {
+                                print("New user created and authenticated in Firebase")
+                                NSUserDefaults.standardUserDefaults().setValue(user?.uid, forKey: KEY_ID)
+                                FIRAuth.auth()?.signInWithEmail(email, password: pwd, completion: nil)
+                                self.performSegueWithIdentifier(SEGUE_LOGGED_IN, sender: nil)
+                            }
+                        })
+                    } else if error!.code == STATUS_WRONG_PASSWORD {
+                        self.showErrorAlert("Could not log in", msg: "Please check your username or password")
+                    }
+                } else {
+                    print("Email user successfully authenticated into Firebase")
+                    NSUserDefaults.standardUserDefaults().setValue(user?.uid, forKey: KEY_ID)
+                    self.performSegueWithIdentifier(SEGUE_LOGGED_IN, sender: nil)
+                }
+            })
+        } else {
+            showErrorAlert("Email and Password Required", msg: "You must enter an email and a password")
+        }
+    }
+    
+    func showErrorAlert(title: String, msg: String) {
+        
+        let alert = UIAlertController(title: title, message: msg, preferredStyle: .Alert)
+        let action = UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil)
+        alert.addAction(action)
+        presentViewController(alert, animated: true, completion: nil)
+    }
 }
+
+
+
+
+
+
+
+
 
